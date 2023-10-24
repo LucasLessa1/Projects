d9e905d191e9aeb3d13ccb088c13f3d87bde79f6
Lucas Lessa <79370622+LucasLessa1@users.noreply.github.com>
2023-10-24 08:59:06 -0300

    Original Files

--- PowerApps/querytest.m
+++ PowerApps/querytest.m
@@ -1,26 +1,13 @@
-Feitos:
-Tomada 2P+T simples, 10 A, para condulete 1"
-Tomada 2P+T simples, 20 A, instalada em condulete 1"
-Tomada 2P+T simples, 10 A, embutida em alvenaria
-Tomada 2P+T simples, 20 A, embutida em alvenaria
-Tomada 2P+T simples, 10 A, instalada diretamente em divisória
-Tomada 2P+T simples, 20 A, instalada diretamente em divisória
-Tomada 2P+T dupla, 10 A, instalada em condulete 1"
-Tomada 2P+T dupla, 20 A, instalada em condulete 1"
-Tomada 2P+T dupla, 10 A, embutida em alvenaria
-Tomada 2P+T dupla, 20 A, embutida em alvenaria
-Tomada 2P+T dupla, 10 A, instalada diretamente em divisória
-Tomada 2P+T dupla, 20 A, instalada diretamente em divisória
-Tomada 2P+T tripla, 10 A, embutida em alvenaria
-Tomada 2P+T tripla, 20 A, embutida em alvenaria
-Tomada 2P+T tripla, 10 A, instalada diretamente em divisória
-Tomada 2P+T tripla, 20 A, instalada diretamente em divisória
-Tomada 2P+T tripla, 10 A, instalada em condulete 1"
-Tomada 2P+T tripla, 20 A, instalada em condulete 1"
-Tomada 2P+T quádrupla, 10 A, embutida em alvenaria
-Tomada 2P+T quádrupla, 20 A, embutida em alvenaria
-Tomada 2P+T quádrupla, 10 A, instalada diretamente em divisória
-Tomada 2P+T quádrupla, 20 A, instalada diretamente em divisória
-Tomada 2P+T 10 A simples em caixa para canaleta de alumínio
-Tomada 2P+T 10 A dupla em caixa para canaleta de alumínio
-Tomada 2P+T 10 A quadrupla em caixa para canaleta ou totem de alumínio
+url='https://org425ee2cf.crm.dynamics.com/api/data/v9.2/crddb_autorowreportinputsessions?$top=10';
+
+% Set the access token
+% accessToken = 'MYTOKEN';
+
+% Create the options for the HTTP request
+options = weboptions('HeaderFields', {'Authorization' ['Bearer ' accessToken]});
+
+% Send the HTTP GET request
+response = webread(url, options);
+
+% Process the response as needed
+disp(response);
--- PowerApps/secauthtest.m
+++ PowerApps/secauthtest.m
@@ -1,134 +1,30 @@
-Tomada 2P+T simples, 10 A, embutida em alvenaria
-Tomada 2P+T dupla, 10 A, embutida em alvenaria
-Tomada 2P+T quádrupla, 10 A, embutida em alvenaria
-Tomada 2P+T simples, 20 A, embutida em alvenaria
-Tomada 2P+T dupla, 20 A, embutida em alvenaria
-Tomada 2P+T quádrupla, 20 A, embutida em alvenaria
-Tomada 2P+T simples, 10 A, embutida em dry-wall
-Tomada 2P+T dupla, 10 A, embutida em dry-wall
-Tomada 2P+T quádrupla, 10 A, embutida em dry-wall
-Tomada 2P+T simples, 20 A, embutida em dry-wall
-Tomada 2P+T dupla, 20 A, embutida em dry-wall
-Tomada 2P+T quádrupla, 20 A, embutida em dry-wall
-Tomada 2P+T simples, 10 A, instalada diretamente em divisória
-Tomada 2P+T dupla, 10 A, instalada diretamente em divisória
-Tomada 2P+T quádrupla, 10 A, instalada diretamente em divisória
-Tomada 2P+T simples, 20 A, instalada diretamente em divisória
-Tomada 2P+T dupla, 20 A, instalada diretamente em divisória
-Tomada 2P+T quádrupla, 20 A, instalada diretamente em divisória
-Tomada 2P+T simples, 10 A, para condulete 1"
-Tomada 2P+T dupla, 10 A, instalada em condulete 1"
-Tomada 2P+T simples, 20 A, instalada em condulete 1"
-Tomada 2P+T dupla, 20 A, instalada em condulete 1"
-Ponto de rede simples, embutido em alvenaria
-Ponto de rede simples, embutido em dry-wall
-Ponto de rede simples, instalado diretamente em divisória
-Ponto de rede simples, instalado em condulete 1"
-Ponto de rede duplo, embutido em alvenaria
-Ponto de rede duplo, embutido em dry-wall
-Ponto de rede duplo, instalado diretamente em divisória
-Ponto de rede duplo, instalado em condulete 1"
-Ponto de rede quádruplo, embutido em alvenaria
-Ponto de rede quádruplo, embutido em dry-wall
-Ponto de rede quádruplo, instalado diretamente em divisória
-Ponto de telefone simples, embutido em alvenaria
-Ponto de telefone simples, embutido em dry-wall
-Ponto de telefone simples, instalado diretamente em divisória
-Ponto de telefone simples, instalado em condulete 1"
-Ponto de telefone duplo, embutido em alvenaria
-Ponto de telefone duplo, embutido em dry-wall
-Ponto de telefone duplo, instalado diretamente em divisória
-Ponto telefônico duplo, instalado em condulete 1"
-Ponto de telefone quádruplo, embutido em alvenaria
-Ponto de telefone quádruplo, embutido em dry-wall
-Ponto de telefone quádruplo, instalado diretamente em divisória
-Interruptor simples, embutido em alvenaria
-Interruptor simples, embutido em dry-wall
-Interruptor simples, instalado diretamente em divisória
-Interruptor simples ou 3W, para condulete 1"
-Interruptor 3W simples, embutido em alvenaria
-Interruptor 3W simples, embutido em dry-wall
-Interruptor 3W simples,  instalado diretamente em divisória
-Interruptor duplo, embutido em alvenaria
-Interruptor duplo, embutido em dry-wall
-Interruptor duplo, instalado diretamente em divisória
-Interruptor duplo, instalado em condulete 1"
-Interruptor 3W duplo, embutido em alvenaria
-Interruptor 3W duplo, embutido em dry-wall
-Interruptor 3W duplo, instalado diretamente em divisória
-Interruptor 3W duplo, instalado em condulete 1"
-Interruptor triplo, embutido em alvenaria
-Interruptor triplo, embutido em dry-wall
-Interruptor triplo, instalado diretamente em divisória
-Interruptor triplo, instalado em condulete 1"
-Interruptor 3W triplo, embutido em alvenaria
-Interruptor 3W triplo, embutido em dry-wall
-Interruptor 3W triplo, instalado diretamente em divisória
-Interruptor 3W triplo, instalado em condulete 1"
-Interruptor 3W+simples, embutido em alvenaria
-Interruptor 3W simples, embutido em dry-wall
-Interruptor 3W+simples, instalado diretamente em divisória
-Interruptor 3W+simples, instalado em condulete 1"
-Interruptor simples + tomada 2P+T 10 A, embutido em alvenaria
-Interruptor simples + tomada 2P+T 10 A, embutido em dry-wall
-Interruptor simples + tomada 2P+T 10 A, instalado diretamente em divisória
-Interruptor simples + tomada 2P+T 10 A, instalado em condulete 1"
-Interruptor simples + tomada 2P+T 20 A, embutido em alvenaria
-Interruptor simples + tomada 2P+T 20 A, embutido em dry-wall
-Interruptor simples + tomada 2P+T 20 A, instalado diretamente em divisória
-Interruptor simples + tomada 2P+T 20 A, instalado em condulete 1"
-Eletroduto de aço galvanizado de 1 1/2" embutido em alvenaria, com demolição e recomposição
-Eletroduto de aço galvanizado de 1 1/4" embutido em alvenaria, com demolição e recomposição
-Eletroduto de aço galvanizado de 1" embutido em alvenaria, com demolição e recomposição
-Eletroduto de aço galvanizado de 2" embutido em alvenaria, com demolição e recomposição
-Eletroduto de aço galvanizado de 3/4" embutido em alvenaria, com demolição e recomposição
-Eletroduto de PVC Corrugado Reforçado 1'' (DE 32mm) embutido em alvenaria, com demolição e recomposição
-Eletroduto de PVC Corrugado Reforçado 3/4'' (DE 25mm) embutido em alvenaria, com demolição e recomposição
-Eletroduto flexível metálico com capa de PVC 1’’ embutido em alvenaria, com demolição e recomposição
-Eletroduto flexível metálico com capa de PVC 3/4" embutido em alvenaria, com demolição e recomposição
-Eletroduto de aço galvanizado de 1 1/2" embutido em dry-wall, com demolição e recomposição
-Eletroduto de aço galvanizado de 1 1/4" embutido em dry-wall, com demolição e recomposição
-Eletroduto de aço galvanizado de 1" embutido em dry-wall, com demolição e recomposição
-Eletroduto de aço galvanizado de 2" embutido em dry-wall, com demolição e recomposição
-Eletroduto de aço galvanizado de 3/4" embutido em dry-wall, com demolição e recomposição
-Eletroduto de PVC Corrugado Reforçado 1'' (DE 32mm) embutido em dry-wall, com demolição e recomposição
-Eletroduto de PVC Corrugado Reforçado 3/4'' (DE 25mm) embutido em dry-wall, com demolição e recomposição
-Eletroduto flexível metálico com capa de PVC 1’’ embutido em dry-wall, com demolição e recomposição
-Eletroduto flexível metálico com capa de PVC 3/4" embutido em dry-wall, com demolição e recomposição
-Abertura e fechamento de visita 60 x 60 cm para instalação no entreforro
-Ponto de dados para AP (access-point) com posicionamento móvel e saída no forro
-Tomada 2P+T tripla, 10 A, embutida em alvenaria
-Tomada 2P+T tripla, 20 A, embutida em alvenaria
-Tomada 2P+T tripla 10 A, embutida em dry-wall
-Tomada 2P+T tripla, 20 A, embutida em dry-wall
-Tomada 2P+T tripla, 10 A, instalada diretamente em divisória
-Tomada 2P+T tripla, 20 A, instalada diretamente em divisória
-Tomada 2P+T tripla, 10 A, instalada em condulete 1"
-Tomada 2P+T tripla, 20 A, instalada em condulete 1"
-Luminária 2x14 W de embutir c/ plugue e tomada 2P+T em condulete
-Luminária 2x14 W de sobrepor c/ plugue e tomada 2P+T em condulete
-Luminária 2x28 W de embutir c/ plugue e tomada 2P+T em condulete
-Luminária 2x28 W de sobrepor c/ plugue e tomada 2P+T em condulete
-Espera para ponto de TV, embutida em alvenaria
-Espera para ponto de TV, embutida em dry-wall
-Espera para ponto de TV, instalada diretamente em divisória
-Infraestrutura para ponto de rede duplo, embutida em alvenaria
-Infraestrutura para ponto de rede duplo, embutida em dry-wall
-Infraestrutura para ponto de rede simples, embutida em alvenaria
-Infraestrutura para ponto de rede simples, embutida em dry-wall
-Ponto de rede simples em caixa para canaleta de alumínio
-Ponto de rede duplo em caixa para canaleta de alumínio
-Tomada 2P+T 10 A simples em caixa para canaleta de alumínio
-Tomada 2P+T dupla em caixa para canaleta de alumínio
-Ponto telefônico simples em caixa para canaleta de alumínio
-Ponto telefônico duplo em caixa para canaleta de alumínio
-Eletroduto de aço galvanizado de 1" para instalação aparente ou diretamente no entreforro ou divisória
-Eletroduto de aço galvanizado de 2" para instalação aparente ou diretamente no entreforro ou divisória
-Eletroduto de PVC Corrugado Reforçado 1'' (DE 32mm) instalado diretamente em divisória
-Eletroduto flexível metálico com capa de PVC 3/4’’ para instalação aparente ou diretamente no entreforro ou divisória
-Eletroduto de aço galvanizado de 1 1/2" para instalação aparente ou diretamente no entreforro ou divisória
-Eletroduto de aço galvanizado de 1 1/4" para instalação aparente ou diretamente no entreforro ou divisória
-Eletroduto de aço galvanizado de 3/4" para instalação aparente ou diretamente no entreforro ou divisória
-Eletroduto de PVC Corrugado Reforçado 3/4'' (DE 25mm) instalado diretamente em divisória
-Eletroduto flexível metálico com capa de PVC 3/4’’ para instalação aparente ou diretamente no entreforro ou divisória
-Tomada 2P+T quadrupla em caixa para canaleta ou totem de alumínio
+% Set the authentication parameters
+clientId = '673d9e85-b0d1-4f5a-ae7a-3dccf0c31a9f';
+clientSecret = 'Fo38Q~tcNdLNzs5rPl5.W6.-fCLubnFIK4v3Vdb.';
+tenantId = 'ec359ba1-630b-4d2b-b833-c8e6d48f8059';
+scope = 'https://org425ee2cf.crm.dynamics.com/.default';
+
+tokenUrl = ['https://login.microsoftonline.com/' tenantId '/oauth2/v2.0/token'];
+
+% Set the request data
+data = ['grant_type=client_credentials', ...
+        '&client_id=', clientId, ...
+        '&client_secret=', clientSecret, ...
+        '&scope=', scope];
+
+% Send the request to obtain the access token
+% options = weboptions('RequestMethod', 'post');
+options=weboptions('HeaderFields',{'Content-Type' 'application/x-www-form-urlencoded'}) ;
+response = webwrite(tokenUrl, data, options);
+accessToken = response.access_token
+
+% Use the access token to make requests to the Power Platform APIs
+% You can include the access token in the Authorization header of your requests
+
+% % Example: Get the list of tables in your environment
+% apiUrl = 'https://org425ee2cf.api.crm.dynamics.com/api/data/v9.1/EntityDefinitions';
+% headers = struct('Authorization', ['Bearer ', accessToken]);
+% response = webread(apiUrl, weboptions('HeaderFields', headers));
+% 
+% % Display the response
+% disp(response);
