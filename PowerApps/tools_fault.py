e4e03150a29fc738301406786a16b4400fda49c4
lucaslessa1 <lucaslessaeco@gmail.com>
2023-12-22 15:10:35 -0300

    Organize the code and put docstrings in functions

--- PowerApps/tools_fault.py
+++ PowerApps/tools_fault.py
@@ -1,19 +1,38 @@
 import pandas as pd
 import numpy as np
 
-
-
 def calculate_length(df):
+    """
+    Calculates the length of a path represented by a DataFrame.
+
+    Args:
+    - df (pandas.DataFrame): DataFrame containing coordinates of a path.
+
+    Returns:
+    - float: Length of the path calculated using the Euclidean distance formula.
+    """
     initial_point = df.iloc[0]
     final_point = df.iloc[-1]
 
-    # Calcula o comprimento usando a fórmula da distância euclidiana
+    # Calculate length using the Euclidean distance formula
     length = np.linalg.norm(final_point - initial_point)
 
     return length
 
 
+
+
 def find_row_with_text(file_path, text):
+    """
+    Finds the line number and content in a file containing the specified text.
+
+    Args:
+    - file_path (str): Path to the file to search.
+    - text (str): Text to search for within the file.
+
+    Returns:
+    - tuple or None: Tuple containing line number and line content if text is found, else returns None.
+    """
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
@@ -23,9 +42,23 @@ def find_row_with_text(file_path, text):
             return None, None  # Return None if the text is not found
     except FileNotFoundError:
         return None, None  # Return None if the file is not found
-    
-    
+
+
+
+
 def modify_file_and_save(input_file, output_file, search_text, replacement_text):
+    """
+    Modifies a file by replacing specified text and saves the modified content to a new file.
+
+    Args:
+    - input_file (str): Path to the input file.
+    - output_file (str): Path to the output file to be created with modifications.
+    - search_text (str): Text to search for in the input file.
+    - replacement_text (str): Text to replace the found search_text.
+
+    Returns:
+    - None
+    """
     try:
         with open(input_file, 'r') as file:
             lines = file.readlines()
